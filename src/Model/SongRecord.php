1f912d4c31f791a7f83f623c8cb01fde7c02105b
Antonello D'Ippolito <antonellodippolito@gmail.com>
2019-03-22 19:58:30 +0100

    Fix problem with empty licenses

--- src/Model/SongRecord.php
+++ src/Model/SongRecord.php
@@ -4,16 +4,37 @@ namespace App\Model;
 
 class SongRecord
 {
+
+    /** @var string */
     public $author;
+
+    /** @var string */
     public $title;
+
+    /** @var string */
     public $duration;
+
+    /** @var \DateTime */
     public $date;
+
+    /** @var string */
     public $link;
+
+    /** @var string|null */
     public $license;
+
+    /** @var string */
     public $service;
 
-    function __construct($author, $title, $duration, $date, $link, $license, $service)
-    {
+    function __construct(
+        string $author,
+        string $title,
+        string $duration,
+        \DateTime $date,
+        string $link,
+        ?string $license,
+        string $service
+    ) {
         $this->author = $author;
         $this->title = $title;
         $this->duration = $duration;
@@ -22,5 +43,4 @@ class SongRecord
         $this->license = $license;
         $this->service = $service;
     }
-
 }
